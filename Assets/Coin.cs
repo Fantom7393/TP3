4c6da22b28c594befee5a804443bec760f8883e8
Fantom7393 <raphael.lapointe99@gmail.com>
2021-11-18 21:27:03 -0500

    Update Coin.cs, Coin.cs.meta, and 117 more files...

---
+++ Assets/Coin.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Coin : MonoBehaviour
+{
+    private AudioSource monAS;
+    private SpriteRenderer spr;
+    private BoxCollider2D col;
+    private PlayableDirector pd;
+    private Animator anim;
+
+    void Start()
+    {
+        monAS = GetComponent<AudioSource>();
+        spr = GetComponent<SpriteRenderer>();
+        col = GetComponent<BoxCollider2D>();
+        pd = GetComponent<PlayableDirector>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        RobotBoyController rbc = collision.GetComponent<RobotBoyController>();
+        if(rbc != null)
+        {
+            ++rbc.PV;
+            DisableComponents();
+            monAS.Play();
+        }
+    }
+
+    private void DisableComponents()
+    {
+        spr.enabled = col.enabled = pd.enabled = anim.enabled = false;
+        Invoke("EnableComponents", 30f);
+    }
+
+    private void EnableComponents()
+    {
+        spr.enabled = col.enabled = pd.enabled = anim.enabled = true;
+    }
+}
